b2f4b892ecfbf2eb570c8ec02ff8292562b4f2ea
unknown <doanddquang98@gmail.com>
2022-08-15 14:16:30 +0700

    first commit

---
+++ configs/mongo-config.js
@@ -0,0 +1,7 @@
+// export mongodb url
+const MONGO_USERNAME = 'MongoDB';
+const MONGO_PASSWORD = '';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'fashion_shop';
+module.exports = "mongodb://127.0.0.1:27017/fashion_shop"
