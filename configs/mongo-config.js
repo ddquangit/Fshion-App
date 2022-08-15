45de1cc2543c725d1269ad4dfc636960324c295d
ddquangit <111245486+ddquangit@users.noreply.github.com>
2022-08-16 03:14:56 +0700

    Update mongo-config.js

--- configs/mongo-config.js
+++ configs/mongo-config.js
@@ -4,6 +4,6 @@ const MONGO_PASSWORD = '';
 const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'fashion_shop';
-module.exports = "mongodb+srv://admin:admin@cluster0.8dxgxer.mongitgodb.net/?retryWrites=true&w=majority/fashion_shop"
+module.exports = "mongodb+srv://admin:admin@cluster0.8dxgxer.mongitgodb.net/fashion_shop"
 // module.exports = "mongodb://127.0.0.1:27017/fashion_shop"
 
