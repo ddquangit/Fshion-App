909d0016a5e37499e6d8ea5766e3c0ea194a13f1
unknown <doanddquang98@gmail.com>
2022-08-15 15:14:00 +0700

    update 1

--- configs/mongo-config.js
+++ configs/mongo-config.js
@@ -4,4 +4,4 @@ const MONGO_PASSWORD = '';
 const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'fashion_shop';
-module.exports = "mongodb://127.0.0.1:27017/fashion_shop"
+module.exports = "mongodb+srv://admin:admin@cluster0.8dxgxer.mongodb.net/fashion_shop"
